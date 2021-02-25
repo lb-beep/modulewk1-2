27ba311eb4846674daf329f174acefeb03c4adcf
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-02-25 09:52:44 -0700

    Update Index.js

--- ModuleTwo/wwwroot/js/Index.js
+++ ModuleTwo/wwwroot/js/Index.js
@@ -1,1 +1,28 @@
-﻿
+$(document).ready(function () {
+    var loginButton = document.createElement("button");
+    $(loginButton).text("Log In");
+
+    var registerButton = document.createElement("button");
+    $(registerButton).text("Register");
+
+    var divContainer = $("nav.navbar div.container");
+
+    $(divContainer).append(loginButton);
+    $(divContainer).append(registerButton);
+
+    $(loginButton).click(function () {
+        alert("you have logged in");
+    });
+
+    $(registerButton).click(function () {
+        var span = document.createElement("span");
+        $(span).text("User");
+        $("#MainContainerDiv").append(span);
+
+        span = document.createElement("span");
+        $(span).text("Password");
+        $("#MainContainerDiv").append(span);
+    });
+}
+);
+
